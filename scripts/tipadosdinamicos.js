aacaac3e7537091e588552cd6875e30d1152dca1
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-15 20:04:41 -0600

    Tipados dinamicos

---
+++ scripts/tipadosdinamicos.js
@@ -0,0 +1,22 @@
+let numero = 5;
+let nombre = "Sebastián";
+let verdaderos = true;
+let falsos = false;
+let nulo = null;
+
+console.log(typeof numero);
+console.log(typeof nombre);
+console.log(typeof verdaderos);
+console.log(typeof falsos);
+console.log(typeof nulo);
+
+//Comentario normal
+
+/**Comentario
+ * de varias lineas
+ * para js
+ */
+
+/* Tambien se puede asi 
+es casi igual que 
+esto */

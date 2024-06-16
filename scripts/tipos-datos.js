fdb10061f3eb5aec77052afcb33dde016e67ca73
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-15 19:50:46 -0600

    Datos primitivos

---
+++ scripts/tipos-datos.js
@@ -0,0 +1,9 @@
+//Datos primitivos
+
+let numero = 1;
+let textos = "estoestexto";
+let verdadero = true;
+let falso = false;
+let sinDefinir;
+let nulo = null;
+let undefinifo = undefined;
---
+++ scripts/variable.js
@@ -0,0 +1,3 @@
+let nombreVariable;
+
+console.log(nombreVariable);

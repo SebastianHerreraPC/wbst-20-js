ca60d175f43f527ef37287db70c4a239e8e28c29
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-16 23:42:30 -0600

    Funciones

---
+++ scripts/funciones.js
@@ -0,0 +1,13 @@
+function saludo() {
+  console.log("Hola ¿Como estas?");
+}
+
+//llamar funcion
+saludo();
+
+function sumas(x, y) {
+  return x + y;
+}
+
+let resultado = sumas(10, 10);
+console.log(resultado);

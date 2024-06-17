a205a8dfb76facb0ec26df99de01b0932bbea071
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-17 00:11:00 -0600

    renombre de carpeta y nuevo directorio

--- tipos/funciones.js
+++ tipos/funciones.js
@@ -6,10 +6,12 @@ function saludo() {
 saludo();
 
 function sumas(x, y) {
+  console.log(arguments);
   return x + y;
 }
 
-let resultado = sumas(10, 10);
+let resultado = sumas(10, 10, 10, 10);
 console.log(resultado);
+console.log(typeof sumas);
 
 //Argumentos son x,y  y parametros los valores que les damos

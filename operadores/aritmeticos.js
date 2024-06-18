a025ba4461cbe5c33f986708d9a2783ad4c0a1fd
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-17 23:18:31 -0600

    operadores aritmeticos

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,19 @@
+//operadores aritmeticos
+let a = 10;
+let b = 20;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(a % b); //modulo o resto
+// console.log(a ** b); //potencia
+
+//incrementar
+console.log(a);
+console.log(++a);
+
+//decrementa
+
+console.log(b);
+console.log(--b);

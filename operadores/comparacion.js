1600c38a38eaab0dd6fddfbbda6c1a5010610aff
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-18 22:46:23 -0600

    operadores comparacion

---
+++ operadores/comparacion.js
@@ -0,0 +1,13 @@
+const a = 5;
+
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+console.log(a == 5);
+console.log(a != 5);
+
+//compara valor y tipo de dato
+console.log(a === "5");
+console.log(a !== "5");

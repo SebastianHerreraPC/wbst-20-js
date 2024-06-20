cd032f375dae2a23d3ce3dc738f7d02bbe719a15
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-19 22:39:11 -0600

    operadores logicos

---
+++ operadores/logicos.js
@@ -0,0 +1,10 @@
+//AND && ambos tienen que ser true
+
+console.log(true && true);
+console.log(false && true);
+
+//OR || si al menos alguno es true
+
+console.log(true || false);
+//NOT !
+console.log(!false);

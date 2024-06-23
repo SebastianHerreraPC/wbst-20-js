aa78884e52392b65b4d2b2ff840613b27453ccea
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-23 17:53:50 -0600

    controles de flujo else if

---
+++ controles-de-flujo/else.js
@@ -0,0 +1,9 @@
+const edad = 15;
+
+if (edad > 17) {
+  console.log("Mayor de edad");
+} else if (edad < 13) {
+  console.log("Es muy menor");
+} else {
+  console.log("Menor de edad");
+}
---
+++ controles-de-flujo/if.js
@@ -0,0 +1,7 @@
+//if (condicion) {expresion }
+
+const edad = 18;
+
+if (edad > 17) {
+  console.log("mayor de edad");
+}

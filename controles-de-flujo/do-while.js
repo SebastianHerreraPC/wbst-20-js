0d68659d524e64f7ab15e4334d90d44b384df0eb
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-23 18:12:52 -0600

    controles de flujo do while

---
+++ controles-de-flujo/do-while.js
@@ -0,0 +1,16 @@
+let i = 2;
+while (i < 2) {
+  if (i % 2 == 0) {
+    console.log("Es par ", i);
+  }
+  i++;
+}
+
+// do {
+//   if (i % 2 == 0) {
+//     console.log("Es par ", i);
+//   }
+//   i++;
+// } while (i < 2);
+
+//primero ejecuta y luego evalua

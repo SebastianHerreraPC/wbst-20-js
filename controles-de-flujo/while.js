e29719b907d039ec411c1033d78ccd1745ed1331
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-23 18:02:41 -0600

    controles de flujo while

---
+++ controles-de-flujo/while.js
@@ -0,0 +1,14 @@
+//loop 1 impresion de numeros pares
+//while(condicion){codigo}
+
+let i = 0;
+
+while (i - 10) {
+  if (i % 2 == 0) {
+    console.log("Es par", i);
+  }
+
+  i++;
+}
+
+console.log("cierre de bucle");
